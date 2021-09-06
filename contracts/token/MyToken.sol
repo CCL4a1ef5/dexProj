8b1caec1fc651220edff92a1e48cac40a3b0a403
chinPractce <chinleongextra@gmail.com>
2021-09-07 01:45:15 +0800

    WalletConnect

--- contracts/token/MyToken.sol
+++ contracts/token/MyToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
 contract MyToken1 is ERC20PresetMinterPauser {
-    constructor() public ERC20PresetMinterPauser("My Token", "MTK1") {}
+    constructor() public ERC20PresetMinterPauser("TEST Token", "TEST1") {}
 }
 
 contract MyToken2 is ERC20PresetMinterPauser {
-    constructor() public ERC20PresetMinterPauser("My Token", "MTK2") {}
+    constructor() public ERC20PresetMinterPauser("TEST Token", "TEST22") {}
 }
